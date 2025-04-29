947f22454806b91bf7936472d722ff7eb6fee773
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-28 21:26:13 -0300

    sucesso

--- teste.py
+++ teste.py
@@ -7,3 +7,43 @@ print("Vamos iniciar o jogo termo.py!")
 
 quantidade_letras = int(input("Qual a dificuldade você gostaria de jogar, 5, 6 ou 7 letras? "))
 
+CINCO_LETRAS = ["nuvem", "poder", "diabo", "nesta", "nariz"]
+SEIS_LETRAS = ["utopia", "casual", "hostil", "anseio", "gentil"]
+SETE_LETRAS = ["mochila", "lasanha", "cozinha", "atacado", "alergia"]
+
+if quantidade_letras == 5:
+    palavra_secreta = random.choice(CINCO_LETRAS)
+elif quantidade_letras == 6:
+    palavra_secreta = random.choice(SEIS_LETRAS)
+elif quantidade_letras == 7:
+    palavra_secreta = random.choice(SETE_LETRAS)
+
+def adivinhacao(quantidade_letras):
+  tentativa = 1
+  letras_descobertas * (quantidade_letras)
+
+  while tentativa <= (quantidade_letras):
+    
+    palpite = input("Digite seu palpite para tentar adivinhar a palavra: ")
+    
+    if len(palpite) != (quantidade_letras):
+        print(f"A palavra não tem {quantidade_letras}, digite novamente")
+        palpite = input("Digite seu palpite para tentar adivinhar a palavra: ")
+
+    if palpite == palavra_secreta:
+        print("Parabéns você acertou a palavra secreta!")
+        break 
+
+    resultado = ''
+    for i in range(quantidade_letras):
+        if palpite[i] = palavra_secreta[i]:
+            letras_descobertas[i] = palpite[i]
+    
+    letras_ja_mostradas = set()
+
+    for letra in palpite:
+        if letra in palavra_secreta and letra not in letras_ja_mostradas:
+            print(f'A palavra contém a letra {letra}')
+
+
+  
