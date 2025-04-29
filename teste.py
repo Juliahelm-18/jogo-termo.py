f26e63c6d3ef48e859c705e29fbb73cef48e14a1
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-29 20:03:21 -0300

    alteração

--- teste.py
+++ teste.py
@@ -5,7 +5,7 @@ nome = input("Digite seu nome para iniciarmos o jogo: ")
 print(f"Bem vindo {nome} antes de iniciar o jogo vamos lhe mostrar as regras: \n- Você deve escolher a dificuldade do jogo, se serão palavras de 5, 6 ou 7 letras; \n- As tentativas são relacionadas com a quantidade de letras, ou seja, 5, 6 ou 7 tentativas; \n- Após o palpite o jogo irá mostrar onde ocorreram acertos e se as letras existem na palavra; \n- O jogo terminará quando você acertar ou quando não houver mais tentativas. ")
 print("Vamos iniciar o jogo termo.py!")
 
-quantidade_letras = int(input("Digite a quantidade de letras que você deseja que a palavra secreta tenha, se serão 5, 6 ou 7 letras: )
+quantidade_letras = int(input("Digite a quantidade de letras que você deseja que a palavra secreta tenha, se serão 5, 6 ou 7 letras: "))
 
 CINCO_LETRAS = ["nuvem", "poder", "diabo", "nesta", "nariz"]
 SEIS_LETRAS = ["utopia", "casual", "hostil", "anseio", "gentil"]
@@ -55,7 +55,8 @@ def jogo(quantidade_letras):
         print(f'Tentativas até agora: {tentativa}')
 
         if tentativa > quantidade_letras:
-        print(f"Você perdeu! A palavra secreta era {palavra_secreta}")
+            print(f"Você perdeu! A palavra secreta era {palavra_secreta}")
+          
 
 
 
